24dc11f33eca37db7d980349091c27c1d4764f45
Tomasz Hein <hein.tomasz@gmail.com>
2019-12-13 16:29:34 +0100

    Cors Test

--- src/main/java/com/RecipeBookBackend/RecipeBookBackendApplication.java
+++ src/main/java/com/RecipeBookBackend/RecipeBookBackendApplication.java
@@ -2,12 +2,27 @@ package com.RecipeBookBackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RecipeBookBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecipeBookBackendApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/products")
+						.allowedMethods("GET", "POST", "PUT", "DELETE")
+						.allowedOrigins("http://localhost:4200");
+			}
+		};
 	}
 
 }
