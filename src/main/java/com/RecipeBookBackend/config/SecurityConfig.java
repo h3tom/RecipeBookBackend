df88aa26594bd61c1aa340101adb1ca308f777ea
Tomasz Hein <hein.tomasz@gmail.com>
2019-12-15 19:59:04 +0100

    Test update v5 (problem with UTF-8 database encoding)

--- src/main/java/com/RecipeBookBackend/config/SecurityConfig.java
+++ src/main/java/com/RecipeBookBackend/config/SecurityConfig.java
@@ -3,13 +3,11 @@ package com.RecipeBookBackend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 import javax.sql.DataSource;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
