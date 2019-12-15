2d475cb6b9d6c5a5df4a22ca3a9c36ec498e127d
Tomasz Hein <hein.tomasz@gmail.com>
2019-12-15 19:54:56 +0100

    Test update v4 (problem with UTF-8 database encoding)

---
+++ src/main/java/com/RecipeBookBackend/config/SecurityInitializer.java
@@ -0,0 +1,6 @@
+package com.RecipeBookBackend.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
