dfbd9cb7b0629240eec5d74246cf6da6fbfd2b14
Tomasz Hein <hein.tomasz@gmail.com>
2019-12-15 20:10:14 +0100

    Test update v7 (problem with UTF-8 database encoding)

--- src/main/java/com/RecipeBookBackend/RecipeBookBackendApplication.java
+++ src/main/java/com/RecipeBookBackend/RecipeBookBackendApplication.java
@@ -3,8 +3,11 @@ package com.RecipeBookBackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class RecipeBookBackendApplication {
@@ -23,6 +26,16 @@ public class RecipeBookBackendApplication {
 						.allowedOrigins("http://localhost:4200");
 			}
 		};
+	}
+
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://eu-cdbr-west-02.cleardb.net/heroku_add31252597c6fd?reconnect=true&serverTimezone=UTC&useUnicode=yes&characterEncoding=UTF-8");
+		dataSource.setUsername("b5cfa3c5452d52");
+		dataSource.setPassword("ba46de3a");
+		return dataSource;
 	}
 
 }
