929f85a5cd73b790d4deab606fe6851b752c6ee1
Tomasz Hein <hein.tomasz@gmail.com>
2020-01-21 16:46:25 +0100

    Updated controller, now it uses RecipeDTO

--- src/main/java/com/RecipeBookBackend/controller/RecipeController.java
+++ src/main/java/com/RecipeBookBackend/controller/RecipeController.java
@@ -1,11 +1,15 @@
 package com.RecipeBookBackend.controller;
 
-import com.RecipeBookBackend.model.Recipe;
+import com.RecipeBookBackend.dto.RecipeDTO;
+import com.RecipeBookBackend.dto.validated.AddRecipeValidation;
 import com.RecipeBookBackend.service.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.groups.Default;
 import java.util.List;
 
 @RestController
@@ -20,18 +24,22 @@ public class RecipeController {
     }
 
     @GetMapping("/{id}")
-    public Recipe getRecipe(@PathVariable Long id) {
+    public RecipeDTO getRecipe(@PathVariable Long id) {
         return recipeService.getRecipe(id);
     }
 
     @GetMapping("/all")
-    public List<Recipe> getAllRecipes() {
+    public List<RecipeDTO> getAllRecipes() {
         return recipeService.getAllRecipes();
     }
 
     @PostMapping(consumes = "application/json")
-    public HttpStatus addRecipe(@RequestBody Recipe recipe) {
-        if (recipeService.addOrUpdateRecipe(recipe)) {
+    public HttpStatus addRecipe(@RequestBody @Validated({AddRecipeValidation.class}) RecipeDTO recipeDTO,
+                                BindingResult result) {
+        if (result.hasErrors()) {
+            return HttpStatus.PRECONDITION_FAILED;
+        }
+        if (recipeService.addOrUpdateRecipe(recipeDTO)) {
             return HttpStatus.OK;
         } else {
             return HttpStatus.PRECONDITION_FAILED;
@@ -39,8 +47,8 @@ public class RecipeController {
     }
 
     @PutMapping(consumes = "application/json")
-    public HttpStatus updateRecipe(@RequestBody Recipe recipe) {
-        if (recipeService.addOrUpdateRecipe(recipe)) {
+    public HttpStatus updateRecipe(@RequestBody @Validated({Default.class}) RecipeDTO recipeDTO) {
+        if (recipeService.addOrUpdateRecipe(recipeDTO)) {
             return HttpStatus.OK;
         } else {
             return HttpStatus.PRECONDITION_FAILED;
