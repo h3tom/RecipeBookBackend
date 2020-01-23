cd7c7bdfb4ad2eddffda54cded87f2303679d2ae
Tomasz Hein <hein.tomasz@gmail.com>
2020-01-23 23:19:12 +0100

    Service now uses BaseConverter interface

--- src/main/java/com/RecipeBookBackend/service/RecipeService.java
+++ src/main/java/com/RecipeBookBackend/service/RecipeService.java
@@ -1,6 +1,6 @@
 package com.RecipeBookBackend.service;
 
-import com.RecipeBookBackend.converter.dto.RecipeConverter;
+import com.RecipeBookBackend.converter.BaseConverter;
 import com.RecipeBookBackend.dto.RecipeDTO;
 import com.RecipeBookBackend.model.Recipe;
 import com.RecipeBookBackend.repository.RecipeRepository;
@@ -15,15 +15,19 @@ import java.util.List;
 public class RecipeService {
 
     private final RecipeRepository recipeRepository;
+    private final BaseConverter<Recipe, RecipeDTO> recipeDTOConverter;
+    private final BaseConverter<RecipeDTO, Recipe> recipeConverter;
 
     @Autowired
-    public RecipeService(RecipeRepository recipeRepository) {
+    public RecipeService(RecipeRepository recipeRepository, BaseConverter<Recipe, RecipeDTO> recipeDTOConverter, BaseConverter<RecipeDTO, Recipe> recipeConverter) {
         this.recipeRepository = recipeRepository;
+        this.recipeDTOConverter = recipeDTOConverter;
+        this.recipeConverter = recipeConverter;
     }
 
     public RecipeDTO getRecipe(Long id) {
         if (recipeRepository.existsById(id)) {
-            return RecipeConverter.getRecipeDTO(recipeRepository.getOne(id));
+            return recipeDTOConverter.convert(recipeRepository.getOne(id));
         } else {
             return null;
         }
@@ -32,14 +36,14 @@ public class RecipeService {
     public List<RecipeDTO> getAllRecipes() {
         if (recipeRepository.count() > 0) {
             List<Recipe> recipes = recipeRepository.findAll();
-            return RecipeConverter.getRecipeDTOS(recipes);
+            return recipeDTOConverter.convertAll(recipes);
         } else {
             return null;
         }
     }
 
     public boolean addOrUpdateRecipe(RecipeDTO recipeDTO) {
-        Recipe recipe = RecipeConverter.getRecipe(recipeDTO);
+        Recipe recipe = recipeConverter.convert(recipeDTO);
         return recipeRepository.save(recipe).getId() != null;
     }
 
