f0a4a29ae1376b72168d6089dbf2edb6a2de8327
Tomasz Hein <hein.tomasz@gmail.com>
2020-01-26 20:04:52 +0100

    Created CustomUserDetailsService

---
+++ src/main/java/com/RecipeBookBackend/security/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.RecipeBookBackend.security;
+
+import com.RecipeBookBackend.model.User;
+import com.RecipeBookBackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+        User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found with username or email : " + usernameOrEmail)
+                );
+
+        return UserPrincipal.create(user);
+    }
+
+}
