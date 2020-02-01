b440ed18582930b3f03520feffba5ace8eaa1afa
Tomasz Hein <hein.tomasz@gmail.com>
2020-02-01 22:29:33 +0100

    Updated JwtAuthenticationResponse (added UserDTO and jwtExpirationInMs fields) and UserService (added new method)

--- src/main/java/com/RecipeBookBackend/dto/response/JwtAuthenticationResponse.java
+++ src/main/java/com/RecipeBookBackend/dto/response/JwtAuthenticationResponse.java
@@ -1,16 +1,22 @@
 package com.RecipeBookBackend.dto.response;
 
+import com.RecipeBookBackend.dto.UserDTO;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class JwtAuthenticationResponse {
-    private String accessToken;
-    private String tokenType = "Bearer";
 
-    public JwtAuthenticationResponse(String accessToken) {
+    private String tokenType = "Bearer";
+    private int jwtExpirationInMs;
+    private String accessToken;
+    private UserDTO user;
+
+    public JwtAuthenticationResponse(String accessToken, UserDTO userDTO, int jwtExpirationInMs) {
         this.accessToken = accessToken;
+        this.user = userDTO;
+        this.jwtExpirationInMs = jwtExpirationInMs;
     }
 
 }
--- src/main/java/com/RecipeBookBackend/service/UserService.java
+++ src/main/java/com/RecipeBookBackend/service/UserService.java
@@ -1,8 +1,11 @@
 package com.RecipeBookBackend.service;
 
+import com.RecipeBookBackend.converter.BaseConverter;
+import com.RecipeBookBackend.dto.UserDTO;
 import com.RecipeBookBackend.model.User;
 import com.RecipeBookBackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,10 +14,12 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final BaseConverter<User,UserDTO> userDTOConverter;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, BaseConverter<User, UserDTO> userDTOConverter) {
         this.userRepository = userRepository;
+        this.userDTOConverter = userDTOConverter;
     }
 
     public boolean existsByUsername(String username) {
@@ -27,5 +32,13 @@ public class UserService {
 
     public boolean save(User user) {
         return userRepository.save(user).getId() != null;
+    }
+
+    public UserDTO getUserDTO(String usernameOrEmail) {
+        User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
+                .orElseThrow(
+                        () -> new UsernameNotFoundException("User not found with username or email : " + usernameOrEmail)
+                );
+        return userDTOConverter.convert(user);
     }
 }
