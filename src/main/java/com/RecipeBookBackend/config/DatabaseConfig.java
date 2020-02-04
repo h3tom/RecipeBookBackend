0ceb67a3c014bd93a8398090d5b360712fc7ae34
Tomasz Hein <hein.tomasz@gmail.com>
2020-02-04 17:59:17 +0100

    Test 6 - I need username, password and driverClassName. I don't know why option with @Value in case of url doesn't work. End of tests

--- src/main/java/com/RecipeBookBackend/config/DatabaseConfig.java
+++ src/main/java/com/RecipeBookBackend/config/DatabaseConfig.java
@@ -1,5 +1,6 @@
 package com.RecipeBookBackend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -9,10 +10,22 @@ import javax.sql.DataSource;
 @Configuration
 public class DatabaseConfig {
 
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(driverClassName);
         dataSource.setUrl("jdbc:mysql://eu-cdbr-west-02.cleardb.net/heroku_add31252597c6fd?reconnect=true&serverTimezone=UTC&useUnicode=yes&characterEncoding=UTF-8");
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
         return dataSource;
     }
 }
